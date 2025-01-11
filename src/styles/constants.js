4f84eb1a9e968ce5bec46b5f569b1879e1280dd0
Jeff Musgrave <jeff.musgrave@gmail.com>
2025-01-11 12:51:30 -0500

    reorganized components

--- src/styles/constants.js
+++ src/styles/constants.js
@@ -1,1 +1,4 @@
-export const siteWidth = "w-[clamp(280px,_90vw,_1152px)]";
+export const siteWidth = "w-[clamp(280px,_90vw,_1140px)]";
+
+export const proseClasses =
+  "prose prose-a:font-bold prose-a:no-underline md:prose-h2:text-4xl max-w-none prose-h2:font-bold z-10  prose-h3:uppercase prose-h3:text-lg  prose-h3:mb-1 prose-h3:font-semibold text-left [&_h2]:scroll-mt-32 dark:prose-invert";
